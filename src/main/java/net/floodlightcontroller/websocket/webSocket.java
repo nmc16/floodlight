e79320f8f0f79ce6f40f5c672eb38fde00e41efd
lukesanderson <lukesanderson@cmail.carleton.ca>
2018-02-19 22:23:34 -0500

    module code for testing purposes

--- src/main/java/net/floodlightcontroller/websocket/webSocket.java
+++ src/main/java/net/floodlightcontroller/websocket/webSocket.java
@@ -4,18 +4,34 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
 import net.floodlightcontroller.storage.IStorageSourceListener;
 
-public class webSocket implements IStorageSourceListener, IFloodlightModule {
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+
+public class webSocket implements IStorageSourceListener, IFloodlightModule {
+	
+	protected IFloodlightProviderService floodlightProvider;
+	protected Set<Long> macAddresses;
+	protected static Logger logger;
+
+	
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
 		// TODO Auto-generated method stub
-		return null;
+		Collection<Class<? extends IFloodlightService>> l =
+		        new ArrayList<Class<? extends IFloodlightService>>();
+		    l.add(IFloodlightProviderService.class);
+		    return l;
 	}
 
 	@Override
@@ -34,6 +50,9 @@ public class webSocket implements IStorageSourceListener, IFloodlightModule {
 	public void init(FloodlightModuleContext context)
 			throws FloodlightModuleException {
 		// TODO Auto-generated method stub
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+	    macAddresses = new ConcurrentSkipListSet<Long>();
+	    logger = LoggerFactory.getLogger(webSocket.class);
 
 	}
 
@@ -41,13 +60,15 @@ public class webSocket implements IStorageSourceListener, IFloodlightModule {
 	public void startUp(FloodlightModuleContext context)
 			throws FloodlightModuleException {
 		// TODO Auto-generated method stub
+		//floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		
 
 	}
 
 	@Override
 	public void rowsModified(String tableName, Set<Object> rowKeys) {
 		// TODO Auto-generated method stub
-
+		System.out.print(tableName);
 	}
 
 	@Override
---
+++ src/main/java/net/floodlightcontroller/websocket/webSocketServer.java
@@ -0,0 +1,6 @@
+package net.floodlightcontroller.websocket;
+
+import javax.websocket.*;
+public class webSocketServer {
+		
+}
